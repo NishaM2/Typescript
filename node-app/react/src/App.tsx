86787e4673a0a043aff31d968e6ede7531f49e61
NishaM2 <nisham2522@gmail.com>
2025-06-18 20:22:46 +0530

    types

---
+++ node-app/react/src/App.tsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Todo title='coding' description='xyz' done={true} />
+    </>
+  )
+}
+
+interface TodoProp {
+  title: string,
+  description: string,
+  done: boolean
+}
+
+function Todo(props: TodoProp) {
+  return <div>
+    <h1>
+      {props.title}
+    </h1>
+    <h2>
+      {props.description}
+    </h2>
+  </div>
+}
+
+
+
+
+export default App
--- node-app/src/a.ts
+++ node-app/src/a.ts
@@ -34,8 +34,8 @@ runAfter1s(function() {
 
 //interface
 interface User {
-    firstname: String,
-    lastname: String,
+    firstname: string,
+    lastname: string,
     age: number
 }
 
@@ -47,7 +47,7 @@ function isLegall(user: User) {
     }
 }
 function greett(user: User) {
-    console.log("hello " + user.firstname);
+    console.log("hellooooo " + user.firstname);
 }
 
 greett({
@@ -55,3 +55,90 @@ greett({
     lastname: "m",
     age: 20
 })
+
+
+//interface
+interface User1 {
+    firstname: string,
+    lastname: string,
+    age: number
+}
+
+//type
+type User2 = {
+    firstname: string,
+    lastname: string,
+    age: number
+}
+
+
+//either number or string
+type hiArg = number | string | boolean;
+function hi(id: hiArg){
+    console.log(`ID: ${id}`)
+}
+hi(1);
+hi("2")
+
+
+//intersection
+type Employee = {
+    name: string,
+    startDate: Date
+};
+
+interface Manager {
+    name: string,
+    department: string
+};
+
+type TeamLead = Employee & Manager;
+
+const teamLead: TeamLead = {
+    name: "nisha",
+    startDate: new Date(),
+    department: "HR"
+};
+
+//arrays
+
+type NumberArr = number[];
+
+function maxvalue(arr: NumberArr) {
+    let max = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+maxvalue([1, 2, 3])
+
+//enums
+enum Direction {
+    up = "up",
+    down = "down",
+    left = "left",
+    right = "right"
+}
+
+function doSomething(keyPressed: Direction) {
+    if(keyPressed == Direction.down) {
+
+    }
+}
+
+doSomething(Direction.down)
+console.log(Direction.down)
+console.log(Direction.up)
+
+//generics
+function getFirstElement<T>(arr: T[]): T {
+    return arr[0];
+}
+const el = getFirstElement(["nisha", "m"]);
+const el1 = getFirstElement([1,2]);
+
+console.log(el.toUpperCase());
